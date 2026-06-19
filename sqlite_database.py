6edf01dcc1ddb9fe21fd3a361d17958bcce96f62
Kameron Stepp <steppk@msoe.edu>
2026-06-19 12:20:36 -0400

    added gitignore and finished up udpated to the databse. Still need to populate card database so i can jsut call udpates to the database

--- sqlite_database.py
+++ sqlite_database.py
@@ -98,6 +98,9 @@ def insert_run_data(data, db_path="slay_the_spire_2_offline.db"):
     con = sqlite3.connect(db_path)
     cur = con.cursor()
     _store_run_data(data, cur)
+    _store_enemy_death(data, cur)
+   # _store_card_stats(data, cur) This is done but need to populate table first
+
     con.commit()
     con.close()
 
@@ -107,7 +110,56 @@ def _store_run_data(data, cursor: sqlite3.Cursor):
         INSERT INTO runs (character_id, floor_reached, run_time_seconds, ascension, won)
         VALUES (?, ?, ?, ?, ?)
          """, (id, data["floor_reached"], data["run_time_sec"], data["ascension"], data["won"]))
+def _store_enemy_death(data, cursor:sqlite3.Cursor):
+    # Get the enemy name and character id related to the death
+    c_id = _get_character_id(data["character_name"], cursor)
+    enemy = data["enemy_name"].replace("ENCOUNTER.", "")
 
+    cursor.execute("""
+        INSERT INTO enemy_deaths (character_id, enemy_name, death_count)
+        VALUES (?, ?, 1)
+        ON CONFLICT(character_id, enemy_name)
+        DO UPDATE SET
+        death_count = death_count + 1
+                   """, (c_id, enemy))
+# Todo card table should already be set up with each card name and id everything else can be 0
+def _store_card_stats(data, cursor: sqlite3.Cursor):
+    c_id = _get_character_id(data["character_name"], cursor)
+    won = 0
+    loss = 0
+    proccessed_cards = {}
+    if data["won"]:
+        won += 1
+    else:
+        loss += 1
+    
+    for card in data["cards"]:
+        name = card["name"].replace("CARD.", "")
+        picked = 0
+        skipped = 0
+        if card["picked"]:
+            picked = 1
+        else:
+            skipped = 1
+        if name not in proccessed_cards:
+            cursor.execute("""
+                UPDATE card_stats
+                SET times_picked = times_picked + ?,
+                times_skipped = times_skipped + ?,
+                deck_wins = deck_wins + ?,
+                deck_losses = deck_losses = ?
+                WHERE ? = character_id AND ? = card_name
+                """, (picked, skipped, won, loss, c_id, name))
+            proccessed_cards.append(name)
+        else:
+            cursor.execute("""
+                UPDATE card_stats
+                SET times_picked = times_picked + ?,
+                times_skipped = times_skipped + ?,
+                WHERE ? = character_id AND ? = card_name
+                """, picked, skipped, c_id, name)
+   
+# This could just be an sql procdeure but a method does the same thing i think
 def _get_character_id(character_name: str, cursor: sqlite3.Cursor):
     name = character_name.replace("CHARACTER.", "").capitalize()
     cursor.execute("""
